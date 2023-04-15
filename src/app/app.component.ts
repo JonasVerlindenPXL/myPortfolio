2e50859d4d9c56cf8080f92e42ed6f52563e70d7
JonasVerlindenPXL <43949146+JonasVerlindenPXL@users.noreply.github.com>
2023-04-15 15:48:34 +0200

    motion of card fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener} from '@angular/core';
+import * as Hammer from 'hammerjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myPortfolio';
+
+  constructor(private el: ElementRef) { }
+  @HostListener('mousemove', ['$event'])
+  onMousemove(event: MouseEvent) {
+    const homeLayoutTextList = this.el.nativeElement.querySelectorAll('.card-glow');
+    homeLayoutTextList.forEach((homeLayoutText: HTMLElement) => {
+      const rect = homeLayoutText.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left - rect.width / 2;
+      const mouseY = event.clientY - rect.top - rect.height / 2;
+      homeLayoutText.style.transform = `perspective(500px) rotateX(${mouseY / -100}deg) rotateY(${mouseX / 100}deg)`;
+    });
+  }
 
 }
--- src/app/pages/activitypage/activitypage.component.ts
+++ src/app/pages/activitypage/activitypage.component.ts
@@ -6,17 +6,5 @@ import {Component, ElementRef, HostListener} from '@angular/core';
   styleUrls: ['./activitypage.component.css']
 })
 export class ActivitypageComponent {
-  private mouseX = 0;
-  private mouseY = 0;
 
-  constructor(private el: ElementRef) { }
-
-  @HostListener('mousemove', ['$event'])
-  onMousemove(event: MouseEvent) {
-    const homeLayoutText = this.el.nativeElement.querySelector('.card-glow');
-    const rect = homeLayoutText.getBoundingClientRect();
-    this.mouseX = event.clientX - rect.left - rect.width / 2;
-    this.mouseY = event.clientY - rect.top - rect.height / 2;
-    homeLayoutText.style.transform = `perspective(5000px) rotateX(${this.mouseY / -20}deg) rotateY(${this.mouseX / 20}deg)`;
-  }
 }
