f5d5be65614d87642293c7b63d148f88f8c98415
JonasVerlindenPXL <43949146+JonasVerlindenPXL@users.noreply.github.com>
2023-04-16 16:35:55 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import {Component, ElementRef, HostListener} from '@angular/core';
 export class AppComponent {
   title = 'myPortfolio';
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
+  }
+
   @HostListener('mousemove', ['$event'])
   onMousemove(event: MouseEvent) {
     const homeLayoutTextList = this.el.nativeElement.querySelectorAll('.card-glow');
@@ -17,6 +19,12 @@ export class AppComponent {
       const mouseX = event.clientX - rect.left - rect.width / 2;
       const mouseY = event.clientY - rect.top - rect.height / 2;
       homeLayoutText.style.transform = `perspective(800px) rotateX(${mouseY / -100}deg) rotateY(${mouseX / 100}deg)`;
+
+      const trailer = this.el.nativeElement.querySelector('.trailer');
+      const x = event.clientX - trailer.offsetWidth / 2;
+      const y = event.clientY - trailer.offsetHeight / 2;
+      trailer.style.transform = `translate(${x}px, ${y}px)`;
+      console.log(`${x} : ${y}`)
     });
   }
 
